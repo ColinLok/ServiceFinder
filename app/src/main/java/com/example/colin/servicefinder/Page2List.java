f7600f5d0ee9f4149a5cab22f206c6a05e5b960e
Chang Chen <vlcpulane2@hotmail.com>
2018-11-08 10:11:02 -0800

    activities built

--- app/src/main/java/com/example/colin/servicefinder/Page2List.java
+++ app/src/main/java/com/example/colin/servicefinder/Page2List.java
@@ -3,12 +3,11 @@ package com.example.colin.servicefinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Page2List extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_page2_list);
     }
 }
--- app/src/main/java/com/example/colin/servicefinder/Page3Detail.java
+++ app/src/main/java/com/example/colin/servicefinder/Page3Detail.java
@@ -3,12 +3,11 @@ package com.example.colin.servicefinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Page3Detail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_page3_detail);
     }
 }
