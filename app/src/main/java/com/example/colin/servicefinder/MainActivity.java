41f9a6a2403d8184526127fd67ee67632fb291a9
Chang Chen <vlcpulane2@hotmail.com>
2018-11-08 13:52:19 -0800

    home page onclick function

--- app/src/main/java/com/example/colin/servicefinder/MainActivity.java
+++ app/src/main/java/com/example/colin/servicefinder/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.colin.servicefinder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
+    View view;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        findViewById(R.id.btn_about).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //在onClick()中启动另一个Activity
+                Intent page2 = new Intent(MainActivity.this,Page2List.class);
+                Intent page3 = new Intent(MainActivity.this,Page3Detail.class);
+                switch (v.getId()) {
+                    case R.id.btn_find_house:
+                        startActivity(page2);
+                        break;
+                    case R.id.btn_about:
+                        startActivity(page3);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        findViewById(R.id.btn_find_house).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //在onClick()中启动另一个Activity
+                Intent page3 = new Intent(MainActivity.this,Page3Detail.class);
+                startActivity(page3);
+            }
+        });
     }
 }
